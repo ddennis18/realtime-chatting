f9fc50a67bc50b4483bdf31b06cd761fd96c5ac3
ddennis18 <osemwonken18@gmail.com>
2025-12-22 20:42:00 +0100

    feat: added React router in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import ChatPage from "./pages/ChatPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
