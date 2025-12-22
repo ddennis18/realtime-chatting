e6e829d4b6b361142f3c69fbac7e50da771853bf
ddennis18 <osemwonken18@gmail.com>
2025-12-22 14:27:17 +0100

    feat: Initialised socket.io and express.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from 'cors'
 
 const PORT = 5000;
 const app = express();
+app.use(cors())
 
 //simple 404 middleware
 app.use((req, res, next) => {
   res.status(404).json({ message: "Route not found" });
-})
+});
 
 //simple error handler
 app.use((err, req, res, next) => {
@@ -14,6 +18,15 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Something went wrong" });
 });
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+// pass the http server to socket.io
+const io = new Server(server, {
+  cors: {
+    // the react server
+    origin: "http://localhost:5173",
+  },
+});
+
+server.listen(PORT, () => {
   console.log(`Server Running on PORT:${PORT}`);
 });
