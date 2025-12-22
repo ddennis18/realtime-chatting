ceb7ea042423335f3f8b118096f1f9f1ff9b4893
ddennis18 <osemwonken18@gmail.com>
2025-12-22 20:34:22 +0100

    refactor{frontend}: Navbar styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,20 @@
-import {MessageCircleCode  } from 'lucide-react'
+import { MessageCircleCode } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <header className="w-full max-w-7xl px-6 py-8 flex justify-between items-center">
-      <div className="text-2xl font-bold text-indigo-600 flex items-center gap-2">
-        <div className="w-8 h-8 bg-indigo-600 rounded-lg flex items-center justify-center">
-          <span className="text-white text-xs"><MessageCircleCode /></span>
+    <div className="w-full bg-slate-50">
+      <header className="mx-auto w-full h-16 max-w-7xl px-6 py-8 flex justify-between items-center">
+        <div className="text-2xl font-bold text-indigo-600 flex items-center gap-2">
+          <div className="w-8 h-8 bg-indigo-600 rounded-lg flex items-center justify-center">
+            <span className="text-white text-xs">
+              <MessageCircleCode />
+            </span>
+          </div>
+          ChatFlow
         </div>
-        ChatFlow
-      </div>
-      <div className="space-x-4">
-      </div>
-    </header>
+        <div className="space-x-4"></div>
+      </header>
+    </div>
   );
 };
 
