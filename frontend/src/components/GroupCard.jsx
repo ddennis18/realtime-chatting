871b8642b3ffa722c6a815ad469ce3300a750409
ddennis18 <osemwonken18@gmail.com>
2025-12-23 16:46:12 +0100

    feat: make the group card able to tell if its the current group and then highlight its self

--- frontend/src/components/GroupCard.jsx
+++ frontend/src/components/GroupCard.jsx
@@ -1,6 +1,24 @@
-const GroupCard = ({ name }) => {
+import { useCallback } from "react";
+import { useChats } from "../context/ChatContext";
+
+const currentStyle = "border-indigo-700 bg-indigo-50/50";
+const defaultStyle = "border-slate-500";
+
+const GroupCard = ({ name, _id }) => {
+  const { currentGroup, setCurrentGroup } = useChats();
+  const isCurrent = currentGroup._id === _id;
+
+  const handleClick = useCallback(() => {
+    setCurrentGroup(_id)
+  });
+
   return (
-    <div className="w-full py-1 px-1 border-l-4 rounded-l-sm border-slate-500">
+    <div
+      className={`w-full py-1 px-1 border-l-4 rounded-sm ${
+        isCurrent ? currentStyle : defaultStyle
+      }`}
+      onClick={handleClick}
+    >
       {name}
     </div>
   );
