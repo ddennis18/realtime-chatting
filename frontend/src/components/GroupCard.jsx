d6d68182a1510b16335f298131bc006382bb5c78
ddennis18 <osemwonken18@gmail.com>
2025-12-23 11:44:24 +0100

    feat: added groupcard component for the sidebar

---
+++ frontend/src/components/GroupCard.jsx
@@ -0,0 +1,9 @@
+const GroupCard = ({ name }) => {
+  return (
+    <div className="w-full py-1 px-1 border-l-4 rounded-l-sm border-slate-500">
+      {name}
+    </div>
+  );
+};
+
+export default GroupCard;
