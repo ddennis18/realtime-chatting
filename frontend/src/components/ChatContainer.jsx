a59c081ac998ca0975d36434c353a7a947928f5a
ddennis18 <osemwonken18@gmail.com>
2025-12-23 09:45:13 +0100

    feat: fixed the chatpage and chat container

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,6 +1,34 @@
-const ChatContainer = () => {
+import { Send } from "lucide-react";
+import MessageBubble from "./MessageBubble";
+
+const ChatContainer = ({
+  messages = [
+    { message: "Hey", createdAt: "12/12/34", sender: "you" },
+    { message: "Hello", createdAt: "12/12/34", sender: "user1" },
+    { message: "Wassup", createdAt: "12/12/34", sender: "user1" },
+  ],
+}) => {
   return (
-    <div className="w-full h-full min-w-[500px] bg-white rounded-lg"></div>
+    <div className="w-full h-full min-w-[300px] flex flex-col">
+      <div className="h-full w-full p-1 bg-indigo-50">
+        <div className="h-full bg-white rounded-lg shadow-lg overflow-y-auto px-4 space-y-1">
+          {messages.map((m) => {
+            return <MessageBubble {...m} />;
+          })}
+        </div>
+      </div>
+      <div className="p-2 flex flex-row gap-1">
+        <input
+          type="text"
+          name=""
+          id=""
+          className="border-2 border-indigo-700 rounded-full w-full p-2"
+        />
+        <button className="bg-indigo-700 font-semibold text-white px-2 rounded-full ">
+          Send
+        </button>
+      </div>
+    </div>
   );
 };
 
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -3,18 +3,10 @@ import Sidebar from "../components/Sidebar";
 
 const ChatPage = () => {
   return (
-    <div className="w-full h-[calc(100vh-56px)] flex justify-center items-center">
-      <div className="h-[80%] bg-slate-50 shadow-xl shadow-indigo-100 p-2 rounded-lg">
-        <div className="flex flex-row justify-between w-full h-full">
-          <Sidebar />
-          <div className="flex flex-col justify-between">
-            <ChatContainer/>
-            <div className="w-full flex flex-row justify-center gap-1 border-t-1 border-slate-500 pt-2 mt-2">
-              <input type="text" placeholder="s" className="border-indigo-500 border-2 rounded-full p-2 w-[80%]"  />
-              <button className="bg-indigo-500 text-white p-2 rounded-full">Send</button>
-            </div>
-          </div>
-        </div>
+    <div className="w-full h-[calc(100vh-64px)] flex flex-row">
+      <Sidebar />
+      <div className="w-full"> 
+        <ChatContainer />
       </div>
     </div>
   );
