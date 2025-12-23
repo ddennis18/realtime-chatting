7bbf5d84111502fa5aeb1ef1bed6db7a60e29384
ddennis18 <osemwonken18@gmail.com>
2025-12-23 16:49:00 +0100

    feat: added set current group logic

--- frontend/src/context/ChatContext.jsx
+++ frontend/src/context/ChatContext.jsx
@@ -10,18 +10,20 @@ import { useUser } from "./UserContext";
 const ChatContext = createContext();
 
 const ChatProvider = ({ children }) => {
-  const [currentGroup, setCurrentGroupState] = useState({});
+  const [currentGroup, setCurrentGroupState] = useState({ name: "Study Group I", _id: "dsjgfhjkl" });
   const [chatList, setChatList] = useState([
     //test DATA
     { message: "Hey", createdAt: "12/12/34", sender: "you", _id: 3 },
     { message: "Hello", createdAt: "12/12/34", sender: "user1", _id: 2 },
     { message: "Wassup", createdAt: "12/12/34", sender: "user1", _id: 1 },
   ]);
-  const { userData } = useUser();
+  const { userData, groups } = useUser();
 
-  const setCurrentGroup = useCallback((g) => {
+  const setCurrentGroup = useCallback((id) => {
     // DO SOME CHAT FETCHING LOGIC HERE
-    setCurrentGroup(g);
+    
+    const group = groups.find(g => g._id === id)
+    setCurrentGroupState(group);
   }, []);
 
   useEffect(() => {}, [userData]);
