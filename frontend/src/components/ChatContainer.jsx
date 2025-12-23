8dd24c0e4f71b55245216085c0a93cb5c92591c5
ddennis18 <osemwonken18@gmail.com>
2025-12-23 16:43:59 +0100

    feat:moved test data to the chat context

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,19 +1,16 @@
 import { Send } from "lucide-react";
 import MessageBubble from "./MessageBubble";
+import { useChats } from "../context/ChatContext";
 
-const ChatContainer = ({
-  messages = [
-    { message: "Hey", createdAt: "12/12/34", sender: "you" },
-    { message: "Hello", createdAt: "12/12/34", sender: "user1" },
-    { message: "Wassup", createdAt: "12/12/34", sender: "user1" },
-  ],
-}) => {
+const ChatContainer = () => {
+  const { chatList } = useChats();
+
   return (
     <div className="w-full h-full min-w-[300px] flex flex-col">
       <div className="h-full w-full p-1 bg-indigo-50">
-        <div className="h-full bg-white rounded-lg shadow-lg overflow-y-auto px-4 space-y-1">
-          {messages.map((m) => {
-            return <MessageBubble {...m} />;
+        <div className="h-full border border-slate-500 bg-white rounded-lg shadow-lg overflow-y-auto py-1 px-4 space-y-1">
+          {chatList.map((m) => {
+            return <MessageBubble key={m._id} {...m} />;
           })}
         </div>
       </div>
@@ -24,7 +21,7 @@ const ChatContainer = ({
           id=""
           className="border-2 border-indigo-700 rounded-full w-full p-2"
         />
-        <button className="bg-indigo-700 font-semibold text-white px-2 rounded-full ">
+        <button className="bg-indigo-700 hover:bg-indigo-700/90  font-semibold text-white px-2 rounded-full ">
           Send
         </button>
       </div>
