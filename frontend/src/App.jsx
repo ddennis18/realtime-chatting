0291b121f9e847e09582e8b23cda35afe0d34eae
ddennis18 <osemwonken18@gmail.com>
2025-12-23 11:08:39 +0100

    feat:Made the user context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 import ChatPage from "./pages/ChatPage";
+import UserProvider, { useUser } from "./context/UserContext";
+import { useEffect } from "react";
 
 function App() {
+  const user = useUser()
+console.log(user)
   return (
-    <div className="min-h-screen">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/chat" element={<ChatPage />} />
-      </Routes>
-    </div>
+      <div className="min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Routes>
+      </div>
   );
 }
 
---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useContext, useState } from "react";
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({ username: "" });
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
+
+export const useUser = () => useContext(UserContext);
