2515a8aaeeca279a7ec239424cfd4936e5371fd3
ddennis18 <osemwonken18@gmail.com>
2025-12-23 09:49:47 +0100

    fix: tailwindcss styling issues in the sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { SidebarIcon } from "lucide-react";
 const Sidebar = () => {
   return (
     <div className="hidden md:block max-w-[200px] min-w-[150px] w-full min-h-[300px]">
-      <div className="h-full p-2 border-r-1 border-1 rounded-lg border-slate-500">
+      <div className="h-full p-2 border rounded-lg border-slate-500">
         <div className="flex flex-row gap-2 items-center">
           <SidebarIcon />
           <h2 className="text-lg font-semibold">Groups</h2>
