7a4e2ae52edcab993a08697abd21ff21102c503f
ddennis18 <osemwonken18@gmail.com>
2025-12-23 11:43:30 +0100

    feat: added issignin and groups state with test data for the user context

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -4,9 +4,16 @@ const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState({ username: "" });
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [groups, setGroups] = useState([
+    { name: "Study Group I", _id: "khgjkl;kh" },
+    { name: "Study Group II", _id: "khgjkl;kh" },
+  ]);
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider
+      value={{ userData: user, setUser, isSignedIn, groups, setGroups }}
+    >
       {children}
     </UserContext.Provider>
   );
