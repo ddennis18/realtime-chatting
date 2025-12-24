225ff0a38367c34fd4149fceface74851a58781a
ddennis18 <osemwonken18@gmail.com>
2025-12-24 07:51:17 +0100

    format: formatted files with prettier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,16 +6,16 @@ import UserProvider, { useUser } from "./context/UserContext";
 import { useEffect } from "react";
 
 function App() {
-  const user = useUser()
-console.log(user)
+  const user = useUser();
+
   return (
-      <div className="min-h-screen">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/chat" element={<ChatPage />} />
-        </Routes>
-      </div>
+    <div className="min-h-screen">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </div>
   );
 }
 
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -5,7 +5,7 @@ const ChatPage = () => {
   return (
     <div className="w-full h-[calc(100vh-64px)] flex flex-row">
       <Sidebar />
-      <div className="w-full"> 
+      <div className="w-full">
         <ChatContainer />
       </div>
     </div>
