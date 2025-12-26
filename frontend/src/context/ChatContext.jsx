d535fc34eb113527affcc3236a18a6d089b9f2be
ddennis18 <osemwonken18@gmail.com>
2025-12-26 09:31:05 +0100

    feat: made the chat context able to set the default current group as the first one in the array and fixed dependencies in the useEffects and use callback

--- frontend/src/context/ChatContext.jsx
+++ frontend/src/context/ChatContext.jsx
@@ -10,23 +10,29 @@ import { useUser } from "./UserContext";
 const ChatContext = createContext();
 
 const ChatProvider = ({ children }) => {
-  const [currentGroup, setCurrentGroupState] = useState({ name: "Study Group I", _id: "dsjgfhjkl" });
+  const [currentGroup, setCurrentGroupState] = useState({});
   const [chatList, setChatList] = useState([
     //test DATA
     { message: "Hey", createdAt: "12/12/34", sender: "you", _id: 3 },
     { message: "Hello", createdAt: "12/12/34", sender: "user1", _id: 2 },
     { message: "Wassup", createdAt: "12/12/34", sender: "user1", _id: 1 },
   ]);
-  const { userData, groups } = useUser();
 
-  const setCurrentGroup = useCallback((id) => {
-    // DO SOME CHAT FETCHING LOGIC HERE
-    
-    const group = groups.find(g => g._id === id)
-    setCurrentGroupState(group);
-  }, []);
+  const { groups } = useUser();
 
-  useEffect(() => {}, [userData]);
+  const setCurrentGroup = useCallback(
+    (id) => {
+      // DO SOME CHAT FETCHING LOGIC HERE
+
+      const group = groups.find((g) => g._id === id);
+      setCurrentGroupState(group);
+    },
+    [groups]
+  );
+
+  useEffect(() => {
+    setCurrentGroup(groups[0]?._id);
+  }, [groups]);
 
   return (
     <ChatContext.Provider value={{ currentGroup, setCurrentGroup, chatList }}>
