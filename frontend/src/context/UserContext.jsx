49bbc6118a343a7fe7cbf5d803c48f09ac51003d
ddennis18 <osemwonken18@gmail.com>
2025-12-26 09:24:04 +0100

    feat: made the usercontext able to set groups when the userdata is fetched

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,21 +1,30 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({ username: "" });
+  const [user, setUser] = useState(null);
   const [isSignedIn, setIsSignedIn] = useState(false);
-  const [groups, setGroups] = useState([
-    { name: "Study Group I", _id: "dsjgfhjkl" },
-    { name: "Study Group II", _id: "khgjkl;kh" },
-    { name: "Study Group III", _id: ";kljhfsvm" },
-    { name: "Study Group IV", _id: "147687yhs" },
-    { name: "Study Group V", _id: "vdbdvsu" },
-  ]);
+  const [groups, setGroups] = useState([]);
+
+  useEffect(() => {
+    if (!user) {
+      setIsSignedIn(false);
+      return;
+    }
+    setIsSignedIn(true);
+    setGroups(user.groups)
+  }, [user]);
 
   return (
     <UserContext.Provider
-      value={{ userData: user, setUser, isSignedIn, groups, setGroups }}
+      value={{
+        userData: user,
+        setUser,
+        isSignedIn,
+        groups,
+        setGroups,
+      }}
     >
       {children}
     </UserContext.Provider>
